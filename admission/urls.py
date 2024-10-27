d6cbbb2842664c852c170488a4ac9fa360573fb4
sumonben <sumonaitso@gmail.com>
2024-10-27 22:07:51 +0600

    Admission form working but not extended layout

--- admission/urls.py
+++ admission/urls.py
@@ -6,11 +6,16 @@ from . import views
 from student import views as  view
 from django.conf import settings
 from django.conf.urls.static import static
+from django import views as django_views
+
 urlpatterns = [
     
     path('/login',views.admissionLogin, name='admission_login'),
     path('/admission-form', views.admissionForm, name="admission_form"),
     path('/admission-form-submit', views.admissionFormSubmit, name="admission_form_submit"),
+
+    path('/jsi18n', django_views.i18n.JavaScriptCatalog.as_view(), name='jsi18n'),
+
 
 
 
